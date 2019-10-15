fedbc048b4c3fe1b3f427bc978bdb443609e179a
Hanna Hurieva <ann.gurjeva@gmail.com>
2019-10-15 15:38:51 +0300

    added substraction operation implementation

--- src/com/ss/Operations.java
+++ src/com/ss/Operations.java
@@ -1,8 +1,12 @@
 package com.ss;
 
-public class Addition {
+public class Operations {
 
-    public int add(int a, int b) {
-        return a + b;
+    public int add(int firstValue, int secondValue) {
+        return firstValue + secondValue;
+    }
+
+    public int sub(int firstValue, int secondValue) {
+        return firstValue - secondValue;
     }
 }
