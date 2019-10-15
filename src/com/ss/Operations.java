dc52a482210cd6b5e1906c282418fed79b58ac6d
Hanna Hurieva <ann.gurjeva@gmail.com>
2019-10-15 16:46:52 +0300

    added add method with 4 parameters

--- src/com/ss/Operations.java
+++ src/com/ss/Operations.java
@@ -3,13 +3,18 @@ package com.ss;
 public class Operations {
 
     public int add(int firstValue, int secondValue) {
-        System.out.println("This is add method");
+        System.out.println("This is add method for 2 params");
         return firstValue + secondValue;
     }
 
     public int add(int firstValue, int secondValue, int thirdValue) {
         System.out.println("This is add method for 3 params");
         return firstValue + secondValue + thirdValue;
+    }
+
+    public int add(int firstValue, int secondValue, int thirdValue, int fourthValue) {
+        System.out.println("This is add method for 4 params");
+        return firstValue + secondValue + thirdValue + fourthValue;
     }
 
     public int sub(int firstValue, int secondValue) {
